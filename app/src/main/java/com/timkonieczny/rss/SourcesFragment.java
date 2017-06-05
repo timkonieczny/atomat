1d0d6a21831be5c58cf5c541dc5564dbcc3261be
timko <timkonieczny@gmail.com>
2017-06-06 00:39:37 +0200

    Added circular reveal animation to FAB in SourcesFragment

--- app/src/main/java/com/timkonieczny/rss/SourcesFragment.java
+++ app/src/main/java/com/timkonieczny/rss/SourcesFragment.java
@@ -1,11 +1,15 @@
 package com.timkonieczny.rss;
 
 
-import android.os.Bundle;
+import android.animation.Animator;
 import android.app.Fragment;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
@@ -15,6 +19,7 @@ import android.widget.GridView;
  */
 public class SourcesFragment extends Fragment {
 
+    private boolean isLayoutRevealed = false;
 
     public SourcesFragment() {
         // Required empty public constructor
@@ -29,9 +34,72 @@ public class SourcesFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         GridView gridView = (GridView) view.findViewById(R.id.sources_grid);
         gridView.setAdapter(new SourcesAdapter(getContext()));
+
+        final View fab = view.findViewById(R.id.add_source_fab);
+        final View revealingView = view.findViewById(R.id.fab_content);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*int[] fabLocation = new int[2];
+                fab.getLocationInWindow(fabLocation);
+                Log.d("SourcesFragment", "InWindow: " + fabLocation[0] + " " + fabLocation[1]);
+                fab.getLocationOnScreen(fabLocation);
+                Log.d("SourcesFragment", "OnScreen: " + fabLocation[0] + " " + fabLocation[1]);*/
+
+                if(isLayoutRevealed){
+                    Animator animator = ViewAnimationUtils.createCircularReveal(
+                            revealingView,
+                            view.getRight(),
+                            view.getBottom(),
+                        /*fabLocation[0]+fab.getWidth()/2,
+                        fabLocation[1]-fab.getHeight()/2,
+                        Math.round(fab.getX()),
+                        Math.round(fab.getY()),*/
+                            (float) Math.hypot(view.getWidth(), view.getHeight()),
+                            0);
+
+                    animator.addListener(new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {}
+
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            revealingView.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animation) {}
+
+                        @Override
+                        public void onAnimationRepeat(Animator animation) {}
+                    });
+                    animator.start();
+                    ((FloatingActionButton)fab).setImageDrawable(getActivity().getDrawable(R.drawable.ic_add_white_24dp));  // TODO: use animation in reverse
+                }else {
+                    Animator animator = ViewAnimationUtils.createCircularReveal(
+                            revealingView,
+                            view.getRight(),
+                            view.getBottom(),
+                        /*fabLocation[0]+fab.getWidth()/2,
+                        fabLocation[1]-fab.getHeight()/2,
+                        Math.round(fab.getX()),
+                        Math.round(fab.getY()),*/
+                            0,
+                            (float) Math.hypot(view.getWidth(), view.getHeight()));
+                    revealingView.setVisibility(View.VISIBLE);
+                    ((FloatingActionButton)fab).setImageDrawable(getActivity().getDrawable(R.drawable.ic_add_to_close));
+                    ((AnimationDrawable)((FloatingActionButton)fab).getDrawable()).start();
+                    animator.start();
+                }
+                isLayoutRevealed = !isLayoutRevealed;
+
+            }
+        });
     }
 }
