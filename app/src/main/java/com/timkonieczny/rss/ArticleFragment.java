21116c3e2683807958abbe24b1092860e47688b9
timko <timkonieczny@gmail.com>
2017-07-18 16:08:04 +0200

    Stripped inline images from links

--- app/src/main/java/com/timkonieczny/rss/ArticleFragment.java
+++ app/src/main/java/com/timkonieczny/rss/ArticleFragment.java
@@ -33,38 +33,26 @@ import android.widget.TextView;
 import org.xml.sax.XMLReader;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+public class ArticleFragment extends Fragment implements ArticleChangedListener, SourceChangedListener{
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class ArticleFragment extends Fragment implements ArticleChangedListener, SourceChangedListener/*, ImageListener*/{
-
-    private Bundle arguments;
-
-    private TextView sourceTitle;
-    private ImageView headerImage;
-    private Uri mostLikelyUrl;
-    private List<Bundle> likelyUrls;
-
-    private CustomTabsClient customTabsClient;
-    private CustomTabsSession customTabsSession;
-    private CustomTabsIntent intent;
-    private CustomTabsIntent.Builder builder;
-    private ImageSpan[] images;
-    private TextView contentTextView;
-    private SpannableStringBuilder spannableStringBuilder;
     private Article article;
+
+    private TextView sourceTitleTextView, contentTextView;
+    private ImageView headerImageView;
+
+    private SpannableStringBuilder spannableStringBuilder;
+    private ImageSpan[] imageSpans;
+    private CustomTabsIntent customTabsIntent;
+
 
     public ArticleFragment() {}
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        arguments = getArguments();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        article = MainActivity.articles.get(getArguments().getInt("index"));
         return inflater.inflate(R.layout.fragment_article, container, false);
     }
 
@@ -83,119 +71,91 @@ public class ArticleFragment extends Fragment implements ArticleChangedListener,
         ((AppCompatActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
         MainActivity.toggle.syncState();*/
 
-        article = MainActivity.articles.get(arguments.getInt("index"));
-
         ActionBar actionBar = ((MainActivity)getActivity()).getSupportActionBar();
         if(actionBar!=null) actionBar.setTitle(article.title);
 
-        headerImage = (ImageView) view.findViewById(R.id.article_header);
-        sourceTitle = (TextView) view.findViewById(R.id.source_title);
+        headerImageView = (ImageView) view.findViewById(R.id.article_header);
+        sourceTitleTextView = (TextView) view.findViewById(R.id.source_title);
 
         ((TextView) view.findViewById(R.id.article_title)).setText(article.title);
         ((TextView) view.findViewById(R.id.article_author)).setText(article.author);
-        sourceTitle.setText(article.source.title);
-
+        sourceTitleTextView.setText(article.source.title);
+        headerImageView.setImageDrawable(article.getImage(this, Article.HEADER));
+        sourceTitleTextView.setCompoundDrawablesWithIntrinsicBounds(article.source.getIconDrawable(this), null, null, null);
         contentTextView = (TextView)view.findViewById(R.id.article_content);
 
-        Html.TagHandler figcaptionHandler = new Html.TagHandler() {
-            int startPosition, endPosition;
-            @Override
-            public void handleTag(boolean opening, String tagName, Editable editable, XMLReader xmlReader) {
-                if(tagName.equalsIgnoreCase("figcaption")){
-                    if(opening) startPosition = editable.length();
-                    else{
-                        endPosition = editable.length();    // 13sp = caption font size; 15sp = body font size
-                        editable.setSpan(new RelativeSizeSpan(13.0f/15), startPosition, endPosition, Spannable.SPAN_MARK_MARK);
-                        editable.setSpan(new ForegroundColorSpan(Color.parseColor("#616161")), startPosition, endPosition, Spannable.SPAN_MARK_MARK);
-                    }
+        spannableStringBuilder = setInlineImages(new SpannableStringBuilder(Html.fromHtml(
+                article.content,Html.FROM_HTML_MODE_COMPACT, null, getCustomTagHandler())));
+        spannableStringBuilder = setInlineUrls(spannableStringBuilder);
+
+        contentTextView.setText(spannableStringBuilder);
+        contentTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        // TODO: Handle non-image media
+    }
+
+    private SpannableStringBuilder setInlineUrls(SpannableStringBuilder spannableStringBuilder){
+        ArrayList<URLSpan> links = new ArrayList<>(Arrays.asList(
+                spannableStringBuilder.getSpans(0, spannableStringBuilder.length(), URLSpan.class)));
+        URLSpan link;
+        // remove links from images
+        int j0 = 0;
+        for(int i = 0; i < links.size(); i++){
+            link = links.get(i);
+            for (int j = j0; j < imageSpans.length; j++){
+                if(spannableStringBuilder.getSpanStart(imageSpans[j]) >= spannableStringBuilder.getSpanStart(link) &&
+                spannableStringBuilder.getSpanEnd(imageSpans[j])<= spannableStringBuilder.getSpanEnd(link)){
+                    spannableStringBuilder.removeSpan(link);
+                    links.remove(i);
+                    i--;
+                    j0 = j+1;
+                    break;
                 }
             }
-        };
-        CharSequence content = Html.fromHtml(article.content, Html.FROM_HTML_MODE_COMPACT, null, figcaptionHandler);
-
-        spannableStringBuilder = new SpannableStringBuilder(content);
-
-        URLSpan[] links = spannableStringBuilder.getSpans(0, content.length(), URLSpan.class);
-        likelyUrls = new ArrayList<>(links.length-1);
-        mostLikelyUrl = null;
-        for(int i = 0; i < links.length; i++) {
-            final Uri linkUri = Uri.parse(links[i].getURL());
+        }
+        // make links clickable
+        List<Bundle> likelyUrls = new ArrayList<>(links.size() - 1);
+        Uri mostLikelyUrl = null;
+        for(int i = 0; i < links.size(); i++) {
+            link = links.get(i);
+            final Uri linkUri = Uri.parse(link.getURL());
             spannableStringBuilder.setSpan(
                     new ClickableSpan() {
                         public void onClick(View view) {
-                            intent.launchUrl(view.getContext(),linkUri);
+                            customTabsIntent.launchUrl(view.getContext(),linkUri);
                         }
                     },
-                    spannableStringBuilder.getSpanStart(links[i]),
-                    spannableStringBuilder.getSpanEnd(links[i]),
-                    spannableStringBuilder.getSpanFlags(links[i])
+                    spannableStringBuilder.getSpanStart(link),
+                    spannableStringBuilder.getSpanEnd(link),
+                    spannableStringBuilder.getSpanFlags(link)
             );
-            spannableStringBuilder.removeSpan(links[i]);
-            if(i<links.length-1){
+            spannableStringBuilder.removeSpan(link);
+            if(i<links.size()-1){
                 Bundle bundle = new Bundle();
                 bundle.putParcelable(CustomTabsService.KEY_URL, linkUri);
                 likelyUrls.add(bundle);
-            }else{
-                mostLikelyUrl = linkUri;
-            }
+            }else mostLikelyUrl = linkUri;
         }
-        if(links.length>0){
-            CustomTabsServiceConnection customTabsServiceConnection = new CustomTabsServiceConnection() {
-                @Override
-                public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
-                    customTabsClient = client;
-                    customTabsClient.warmup(0);
-                    customTabsSession = customTabsClient.newSession(new CustomTabsCallback());
-                    if(mostLikelyUrl != null) customTabsSession.mayLaunchUrl(mostLikelyUrl, null, likelyUrls);  // TODO: Maybe loading too many links
+        if(links.size()>0) CustomTabsClient.bindCustomTabsService(getContext(), "com.android.chrome",
+                    getCustomTabsServiceConnection(mostLikelyUrl, likelyUrls));
 
-                    builder = new CustomTabsIntent.Builder(customTabsSession);
-                    intent = builder.build();
-                }
+        return spannableStringBuilder;
+    }
 
-                @Override
-                public void onServiceDisconnected(ComponentName name) {
-                    customTabsClient = null;
-                }
-            };
-
-            CustomTabsClient.bindCustomTabsService(getContext(), "com.android.chrome", customTabsServiceConnection);
-        }
-        contentTextView.setText(spannableStringBuilder);
-        contentTextView.setMovementMethod(LinkMovementMethod.getInstance());
-
-        images = spannableStringBuilder.getSpans(0, content.length(), ImageSpan.class);
-
+    private SpannableStringBuilder setInlineImages(SpannableStringBuilder spannableStringBuilder){
+        imageSpans = spannableStringBuilder.getSpans(0, spannableStringBuilder.length(), ImageSpan.class);
 
         if(article.inlineImages == null){
-            article.inlineImages = new Image[images.length];
+            article.inlineImages = new Image[imageSpans.length];
             for(int i = 0; i < article.inlineImages.length; i++){
                 article.inlineImages[i] = new Image();
-                article.inlineImages[i].url = images[i].getSource();
+                article.inlineImages[i].url = imageSpans[i].getSource();
             }
         }
 
         for(int i = 0; i < article.inlineImages.length; i++){
-            if(article.getImage(this, i) != null){
-                setInlineImage(i);
-            }
+            if(article.getImage(this, i) != null) setInlineImage(i);
         }
-
-        // TODO: Handle non-image media
-
-        headerImage.setImageDrawable(article.getImage(this, Article.HEADER));
-
-        sourceTitle.setCompoundDrawablesWithIntrinsicBounds(article.source.getIconDrawable(this), null, null, null);
-    }
-
-    @Override
-    public void onSourceChanged(Source source) {
-        sourceTitle.setCompoundDrawablesWithIntrinsicBounds(source.icon.drawable, null, null, null);
-    }
-
-    @Override
-    public void onArticleChanged(Article article, int flag) {
-        if(flag == Article.HEADER) headerImage.setImageDrawable(article.header.drawable);
-        else setInlineImage(flag);
+        return spannableStringBuilder;
     }
 
     private void setInlineImage(int index){
@@ -206,10 +166,63 @@ public class ArticleFragment extends Fragment implements ArticleChangedListener,
         article.inlineImages[index].drawable.setBounds(0, 0, imageWidth, imageHeight);
         spannableStringBuilder.setSpan(
                 new ImageSpan(article.inlineImages[index].drawable),
-                spannableStringBuilder.getSpanStart(images[index]),
-                spannableStringBuilder.getSpanEnd(images[index]),
-                spannableStringBuilder.getSpanFlags(images[index]));
-        spannableStringBuilder.removeSpan(images[index]);
-        contentTextView.setText(spannableStringBuilder);
+                spannableStringBuilder.getSpanStart(imageSpans[index]),
+                spannableStringBuilder.getSpanEnd(imageSpans[index]),
+                spannableStringBuilder.getSpanFlags(imageSpans[index]));
+        spannableStringBuilder.removeSpan(imageSpans[index]);
+    }
+
+    private Html.TagHandler getCustomTagHandler(){
+        return new Html.TagHandler() {
+            int startPosition, endPosition;
+            @Override
+            public void handleTag(boolean opening, String tagName, Editable editable, XMLReader xmlReader) {
+                if(tagName.equalsIgnoreCase("figcaption")){
+                    if(opening) startPosition = editable.length();
+                    else{
+                        endPosition = editable.length();    // 13sp = caption font size; 15sp = body font size
+                        editable.setSpan(new RelativeSizeSpan(13.0f/15), startPosition, endPosition, Spannable.SPAN_MARK_MARK); // TODO: apply caption color with respect to DayNight theme
+                        editable.setSpan(new ForegroundColorSpan(Color.parseColor("#616161")), startPosition, endPosition, Spannable.SPAN_MARK_MARK);
+                    }
+                }
+            }
+        };
+    }
+
+    private CustomTabsServiceConnection getCustomTabsServiceConnection(final Uri mostLikelyUrl, final List<Bundle> likelyUrls){
+        return new CustomTabsServiceConnection() {
+
+            private CustomTabsClient client;
+            private CustomTabsSession session;
+
+            @Override
+            public void onCustomTabsServiceConnected(ComponentName name, CustomTabsClient client) {
+                this.client = client;
+                this.client.warmup(0);
+                session = this.client.newSession(new CustomTabsCallback());
+                session.mayLaunchUrl(mostLikelyUrl, null, likelyUrls);  // TODO: Maybe loading too many links
+
+                customTabsIntent = (new CustomTabsIntent.Builder(session)).build();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                client = null;
+            }
+        };
+    }
+
+    @Override
+    public void onSourceChanged(Source source) {
+        sourceTitleTextView.setCompoundDrawablesWithIntrinsicBounds(source.icon.drawable, null, null, null);
+    }
+
+    @Override
+    public void onArticleChanged(Article article, int flag) {
+        if(flag == Article.HEADER) headerImageView.setImageDrawable(article.header.drawable);
+        else {
+            setInlineImage(flag);
+            contentTextView.setText(spannableStringBuilder);
+        }
     }
 }
