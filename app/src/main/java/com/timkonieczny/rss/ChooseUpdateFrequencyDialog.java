78e8be437a4288e8330d57c3753bd89bd722c4c9
Tim Konieczny <timkonieczny@gmail.com>
2017-08-12 00:17:10 +0200

    Added single-choice list to update frequency dialog

--- app/src/main/java/com/timkonieczny/rss/ChooseUpdateFrequencyDialog.java
+++ app/src/main/java/com/timkonieczny/rss/ChooseUpdateFrequencyDialog.java
@@ -11,49 +11,37 @@ public class ChooseUpdateFrequencyDialog extends DialogFragment {
 
     FragmentManager fragmentManager;
     Source source;
+    int selectedItem = -1;
+
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        final long[] updateFrequencies = new long[]{3600000, 7200000, 10800000, 21600000, 43200000, 86400000, 900000};
+
+        for(int i = 0; i < updateFrequencies.length; i++){
+            if(updateFrequencies[i] == source.updateFrequency){
+                selectedItem = i;
+                break;
+            }
+        }
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Update " + source.title)
-                .setItems(R.array.update_freq_values, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // The 'which' argument contains the index position
-                        // of the selected item
-                        switch (which){
-                            case 0:
-                                source.updateFrequency = 3600000;
-                                source.changeBackgroundUpdateFrequency();
-                                break;
-                            case 1:
-                                source.updateFrequency = 7200000;
-                                source.changeBackgroundUpdateFrequency();
-                                break;
-                            case 2:
-                                source.updateFrequency = 10800000;
-                                source.changeBackgroundUpdateFrequency();
-                                break;
-                            case 3:
-                                source.updateFrequency = 21600000;
-                                source.changeBackgroundUpdateFrequency();
-                                break;
-                            case 4:
-                                source.updateFrequency = 43200000;
-                                source.changeBackgroundUpdateFrequency();
-                                break;
-                            case 5:
-                                source.updateFrequency = 86400000;
-                                source.changeBackgroundUpdateFrequency();
-                                break;
-                            case 6:
-                                source.updateFrequency = 900000;
-                                source.changeBackgroundUpdateFrequency();
-                                break;
-                        }
-                    }
-                })
         .setCancelable(true)
-        .setNegativeButton(R.string.cancel, null);
+        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                source.updateFrequency = updateFrequencies[selectedItem];
+                source.changeBackgroundUpdateFrequency();
+            }
+        })
+        .setNegativeButton(R.string.cancel, null)
+        .setSingleChoiceItems(R.array.update_freq_values, selectedItem, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                selectedItem = which;
+            }
+        });
         return builder.create();
     }
 
--- app/src/main/java/com/timkonieczny/rss/UpdateService.java
+++ app/src/main/java/com/timkonieczny/rss/UpdateService.java
@@ -2,10 +2,12 @@ package com.timkonieczny.rss;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.util.Log;
 
 public class UpdateService extends JobService {
     @Override
     public boolean onStartJob(final JobParameters jobParameters) {
+        Log.d("UpdateService", "onStartJob()");
         new BackgroundFeedTask(){
             @Override
             protected void onPostExecute(Void aVoid){
